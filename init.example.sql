6591239c219bcbb0867075c7638aa186a11e97a1
pandamin <whfk233@gmail.com>
2024-11-24 16:48:50 +0330

    fix: Fix sqlserver container's faulty health check

--- init.example.sql
+++
@@ -1,9 +0,0 @@
-USE [master];
-GO
-
-IF NOT EXISTS (SELECT * FROM sys.sql_logins WHERE name = 'admin')
-BEGIN
-    CREATE LOGIN [newuser] WITH PASSWORD = 'admin password', CHECK_POLICY = OFF;
-    ALTER SERVER ROLE [sysadmin] ADD MEMBER [newuser];
-END
-GO
