6ee00adcbdf7d13f5962fe9f52a2823cd8ece4c6
pandamin <whfk233@gmail.com>
2024-11-24 22:20:09 +0330

    feat: Add FirstName and LastName fields to User

--- ProductManager.Domain/Entities/User.cs
+++ ProductManager.Domain/Entities/User.cs
@@ -5,4 +5,6 @@ namespace ProductManager.Domain.Entities;
 public class User : IdentityUser
 {
     public List<Product> Products { get; set; } = [];
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
 }
--- ProductManager.Infrastructure/Migrations/20241124184731_AddUserFields.Designer.cs
+++ ProductManager.Infrastructure/Migrations/20241124184731_AddUserFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductManager.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using ProductManager.Infrastructure.Persistence;
 namespace ProductManager.Infrastructure.Migrations
 {
     [DbContext(typeof(ProductManagerDbContext))]
-    partial class ProductManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241124184731_AddUserFields")]
+    partial class AddUserFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
