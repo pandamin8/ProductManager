bacef6af2bbf6a99ecbb4e22a3e2cf7a952d51e0
pandamin <whfk233@gmail.com>
2024-11-25 18:30:39 +0330

    fix: Add IUserContext and HttpContextAccessor to the dependency injection

--- ProductManager.Application/Extensions/ServiceCollectionExtensions.cs
+++ ProductManager.Application/Extensions/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@ using FluentValidation;
 using FluentValidation.AspNetCore;
 using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
+using ProductManager.Application.Users;
 
 namespace ProductManager.Application.Extensions;
 
@@ -12,11 +13,17 @@ public static class ServiceCollectionExtensions
         var applicationAssembly = typeof(ServiceCollectionExtensions).Assembly;
 
         services.AddMediatR(options => options.RegisterServicesFromAssembly(applicationAssembly));
+
+        services.AddLogging();
+        
         services.AddAutoMapper(applicationAssembly);
         
         services.AddValidatorsFromAssembly(applicationAssembly)
             .AddFluentValidationAutoValidation();
         
         services.AddFluentValidationRulesToSwagger();
+        
+        services.AddScoped<IUserContext, UserContext>();
+        services.AddHttpContextAccessor();
     }
 }
